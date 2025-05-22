a2ab0859fe44b17b87ff0eb2d2ced2a9694550a7
DaniMengi <danimengii@gmail.com>
2025-05-22 12:12:02 +0200

    AI rivista, spawner e destination

--- Assets/script/EntityBehavior.cs
+++ Assets/script/EntityBehavior.cs
@@ -1,4 +1,6 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 using UnityEngine.AI;
 
 public class EntityBehavior : MonoBehaviour
@@ -20,8 +22,25 @@ public class EntityBehavior : MonoBehaviour
     private NavMeshAgent agent;
     private GameManager gm;
 
+
+    public Transform[] vicesDestinations;
+    public Transform[] VirtuesDestinations;
+
+    private float elapsedTime = 0;
+    private float targetEvaluationTIme = 2f;
+
     void Start()
     {
+        if (entityType == EntityType.Virtue)
+        {
+            transform.position = VirtuesDestinations[Random.Range(0, VirtuesDestinations.Length)].position;
+        }
+        else
+        {
+            transform.position = vicesDestinations[Random.Range(0, vicesDestinations.Length)].position;
+
+        }
+
         agent = GetComponent<NavMeshAgent>();
         agent.speed = speed;
 
@@ -32,11 +51,13 @@ public class EntityBehavior : MonoBehaviour
                 player = playerObj.transform;
         }
 
-        gm = FindObjectOfType<GameManager>();
+        gm = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
     }
 
     void Update()
     {
+        elapsedTime += Time.deltaTime;
+
         if (player == null || agent == null)
             return;
 
@@ -44,24 +65,33 @@ public class EntityBehavior : MonoBehaviour
 
         if (entityType == EntityType.Virtue)
         {
-            // Le virtù scappano sempre dal player
-            FleeFromPlayer();
+            if (elapsedTime % targetEvaluationTIme > 1)
+            {
+                // Le virtù scappano sempre dal player
+                if (agent.remainingDistance < 1)
+                {
+                    FleeFromPlayer();
+                }
+            }
         }
         else if (entityType == EntityType.Vice)
         {
-            bool chaseCondition = gm != null && (gm.collectedVirtuesCount >= chaseThreshold || gm.collectedVicesCount >= chaseThreshold);
+            if (elapsedTime % targetEvaluationTIme > 1)
+            {
+                bool chaseCondition = gm != null && (gm.collectedVirtuesCount >= chaseThreshold || gm.collectedVicesCount >= chaseThreshold);
 
-            if (chaseCondition)
-            {
-                ChasePlayer();
-            }
-            else if (distance < triggerDistance)
-            {
-                FleeFromPlayer();
-            }
-            else
-            {
-                Wander(); // Movimento casuale se non c'è il player vicino
+                if (chaseCondition)
+                {
+                    ChasePlayer();
+                }
+                else if (distance < triggerDistance)
+                {
+                    FleeFromPlayer();
+                }
+                else
+                {
+                    Wander(); // Movimento casuale se non c'è il player vicino
+                }
             }
         }
     }
@@ -74,21 +104,51 @@ public class EntityBehavior : MonoBehaviour
 
     void FleeFromPlayer()
     {
-        Vector3 fleeDirection = (transform.position - player.position).normalized;
-        Vector3 fleeTarget = transform.position + fleeDirection * triggerDistance;
-        agent.SetDestination(fleeTarget);
+        //Vector3 fleeDirection = (transform.position - player.position).normalized;
+        //Vector3 fleeTarget = transform.position + fleeDirection * triggerDistance;
+        //agent.SetDestination(fleeTarget);
+        List<Transform> potentialTargets = new List<Transform>();
+
+        foreach (Transform t in VirtuesDestinations)
+        {
+            if (Vector3.Distance(t.position, player.position) > 20) 
+            { 
+                potentialTargets.Add(t);
+            }
+        }
+
+        agent.SetDestination(potentialTargets[Random.Range(0, potentialTargets.Count)].position);
     }
 
     void Wander()
     {
         if (!agent.hasPath || agent.remainingDistance < 1f)
         {
-            Vector3 randomDirection = Random.insideUnitSphere * triggerDistance;
-            randomDirection += transform.position;
-            NavMeshHit hit;
-            if (NavMesh.SamplePosition(randomDirection, out hit, triggerDistance, NavMesh.AllAreas))
+            //Vector3 randomDirection = Random.insideUnitSphere * triggerDistance;
+            //randomDirection += transform.position;
+            //NavMeshHit hit;
+            //if (NavMesh.SamplePosition(randomDirection, out hit, triggerDistance, NavMesh.AllAreas))
+            //{
+            //    agent.SetDestination(hit.position);
+            //}
+
+            if (entityType == EntityType.Virtue)
             {
-                agent.SetDestination(hit.position);
+                int randomLocationIndex = Random.Range(0, VirtuesDestinations.Length);
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(VirtuesDestinations[randomLocationIndex].position, out hit, triggerDistance, NavMesh.AllAreas))
+                {
+                    agent.SetDestination(hit.position);
+                }
+            }
+            else
+            {
+                int randomLocationIndex = Random.Range(0, vicesDestinations.Length);
+                NavMeshHit hit;
+                if (NavMesh.SamplePosition(vicesDestinations[randomLocationIndex].position, out hit, triggerDistance, NavMesh.AllAreas))
+                {
+                    agent.SetDestination(hit.position);
+                }
             }
         }
     }
@@ -103,4 +163,5 @@ public class EntityBehavior : MonoBehaviour
         }
     }
 }
+
 
